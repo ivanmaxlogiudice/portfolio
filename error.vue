b00facb0c70d59183017846ff7f1a0be6c71bd5c
Iván Máximiliano, Lo Giudice <ivanmaxlogiudice@gmail.com>
2023-10-31 20:13:46 -0300

    feat: add error page

---
+++ error.vue
@@ -0,0 +1,26 @@
+<template>
+    <NuxtLayout name="basic">
+        <h1 class="text-8xl font-bold text-primary">
+            404
+        </h1>
+
+        <h2 class="text-3xl font-semibold">
+            Page not found
+        </h2>
+
+        <p class="mt-6">
+            Sorry, we couldn't find the page you're looking for.
+        </p>
+
+        <NuxtLink to="/" class="mt-6 rounded-md bg-primary-500 px-4 py-2 font-semibold text-white transition duration-250 ease">
+            Go Home
+        </NuxtLink>
+    </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+useSeoMeta({
+    description: 'Sorry, we couldn\'t find the page you\'re looking for.',
+    title: 'Page not found',
+})
+</script>
---
+++ layouts/basic.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="min-h-screen flex flex-col font-rubik text-gray-800 antialiased dark:bg-gray-900 dark:text-gray-200">
+        <main class="flex flex-1 flex-col items-center justify-center">
+            <slot />
+        </main>
+    </div>
+</template>
