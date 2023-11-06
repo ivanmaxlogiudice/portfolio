8f48c41af8a97447fbf82a8c421577a8ff656520
Iván Máximiliano, Lo Giudice <ivanmaxlogiudice@gmail.com>
2023-11-05 21:32:55 -0300

    fix: og image url

--- composables/meta.ts
+++ composables/meta.ts
@@ -10,6 +10,7 @@ export function setMeta(options: MetaOptions) {
     const { title = '', description = '', image = '/images/og.webp' } = options
     const { baseURL } = useRuntimeConfig().public
     const url = new URL(useRoute().path, baseURL).toString()
+    const imageUrl = new URL(image, baseURL).toString()
 
     useHead({
         link: [{ href: url, rel: 'canonical' }],
@@ -23,11 +24,11 @@ export function setMeta(options: MetaOptions) {
         ogDescription: description,
         ogUrl: url,
         ogType: 'website',
-        ogImage: image,
+        ogImage: imageUrl,
 
         twitterTitle: title,
         twitterDescription: description,
-        twitterImage: image,
+        twitterImage: imageUrl,
         twitterCard: 'summary_large_image',
     })
 }
